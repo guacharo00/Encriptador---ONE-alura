d46a2b723abde49bb9cd7ea05d60a40ba2fc0ef0
Luis Fructuoso <lfcafn@gmail.com>
2022-01-19 16:46:47 -0400

    Maquetacion terminada completamente

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Fecha
+const footer = document.querySelector("footer");
+const fecha = new Date().getFullYear();
+const footer_text = `
+  <p class="footer_text">
+  Creado por <span>Luis Fructuoso</span> &copy;Todos los derechos
+  reservados (${fecha})
+  </p>
+`;
+footer.innerHTML = footer_text;
+// Auto ajuste del textarea
 const textarea = document.querySelectorAll("textarea");
 textarea.forEach((area, i) => {
   area.addEventListener("keyup", (e) => {
